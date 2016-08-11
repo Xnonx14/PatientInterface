5212ecbb15af393854267062059a9e167743a049
Kevin Chang <Kchang@kevins-mac-mini.fios-router.home>
2016-08-11 01:44:33 -0400

    Added functionality to the clearAll button. first page is cleared.

--- PatientInterface/FirstViewController.swift
+++ PatientInterface/FirstViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
     var defaultColor: UIColor!
     var list = Array<UIButton>();
+    @IBOutlet weak var switchGender: UISegmentedControl!
+    @IBOutlet weak var tfAge: UITextField!
     @IBOutlet weak var btnHTN: UIButton!
     @IBOutlet weak var btnDM: UIButton!
     @IBOutlet weak var btnHLD: UIButton!
     @IBOutlet weak var btnNone: UIButton!
+    @IBOutlet weak var btnClearAll: UIButton!
+    @IBAction func clearAll(sender: UIButton) {
+        deselectAllButtons();
+        tfAge.text = "";
+        switchGender.selectedSegmentIndex = 0;
+    }
     @IBAction func pressNone(sender: UIButton) {
+        registerButton(sender);
         if(sender.currentTitleColor != defaultColor){
             sender.setTitleColor(defaultColor, forState: UIControlState.Normal)
             return;
@@ -33,22 +42,19 @@ class FirstViewController: UIViewController {
             list.append(btn);
         }
     }
-    func deselectAllButtons(btn: UIButton){
+    func deselectAllButtons(){
         let size = list.count;
-        let indexToAvoid = list.indexOf(btn);
         for i in 0..<size{
-            if(i==indexToAvoid){
-                continue;
-            }
             list[i].setTitleColor(defaultColor, forState: UIControlState.Normal)
         }
     }
     @IBAction func btnPressed(sender: UIButton) {
+        registerButton(sender);
         if(sender.currentTitleColor != defaultColor){
             sender.setTitleColor(defaultColor, forState: UIControlState.Normal)
         }else{
             sender.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Normal)
-            if(sender != btnNone){
+            if(sender != btnNone && (btnHTN==sender || btnDM == sender || btnHLD == sender)){
                 btnNone.setTitleColor(defaultColor, forState: UIControlState.Normal)
             }
         }
@@ -56,9 +62,15 @@ class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tfAge.keyboardType = UIKeyboardType.NumberPad;
         if(defaultColor==nil){
             defaultColor = btnHTN.currentTitleColor;
         }
+        self.tfAge.delegate = self;
+    }
+    func textFieldShouldReturn(textField: UITextField)->Bool {
+        self.view.endEditing(true);
+        return false;
     }
     
 
