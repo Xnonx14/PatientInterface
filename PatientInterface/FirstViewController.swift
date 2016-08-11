14c645c93efe23f7173c45c26fed5463b6a202d5
Kevin Chang <Kchang@kevins-mac-mini.fios-router.home>
2016-08-11 01:03:12 -0400

    Clicking None will deselect the previous buttons.

--- PatientInterface/FirstViewController.swift
+++ PatientInterface/FirstViewController.swift
@@ -9,15 +9,61 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    var defaultColor: UIColor!
+    var list = Array<UIButton>();
+    @IBOutlet weak var btnHTN: UIButton!
+    @IBOutlet weak var btnDM: UIButton!
+    @IBOutlet weak var btnHLD: UIButton!
+    @IBOutlet weak var btnNone: UIButton!
+    @IBAction func pressNone(sender: UIButton) {
+        if(sender.currentTitleColor != defaultColor){
+            sender.setTitleColor(defaultColor, forState: UIControlState.Normal)
+            return;
+        }
+        btnHTN.setTitleColor(defaultColor, forState: UIControlState.Normal)
+        btnDM.setTitleColor(defaultColor, forState: UIControlState.Normal)
+        btnHLD.setTitleColor(defaultColor, forState: UIControlState.Normal)
+        sender.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Normal)
+    }
+    
+    func registerButton(btn: UIButton){
+        if(list.contains(btn)){
+            return;
+        }else{
+            list.append(btn);
+        }
+    }
+    func deselectAllButtons(btn: UIButton){
+        let size = list.count;
+        let indexToAvoid = list.indexOf(btn);
+        for i in 0..<size{
+            if(i==indexToAvoid){
+                continue;
+            }
+            list[i].setTitleColor(defaultColor, forState: UIControlState.Normal)
+        }
+    }
+    @IBAction func btnPressed(sender: UIButton) {
+        if(sender.currentTitleColor != defaultColor){
+            sender.setTitleColor(defaultColor, forState: UIControlState.Normal)
+        }else{
+            sender.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Normal)
+            if(sender != btnNone){
+                btnNone.setTitleColor(defaultColor, forState: UIControlState.Normal)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if(defaultColor==nil){
+            defaultColor = btnHTN.currentTitleColor;
+        }
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
